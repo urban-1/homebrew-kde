12ea85c29294a5c80d0f836cffea1684686be647
andreas.bontozoglou <andreas.bontozoglou@sky.uk>
2018-01-24 11:05:56 +0000

    Fix kf5-sonnet.rb patch conflict

--- kf5-sonnet.rb
+++ kf5-sonnet.rb
@@ -49,11 +49,9 @@ index f8c99d1..f2fa4a9 100644
 --- a/src/plugins/nsspellchecker/CMakeLists.txt
 +++ b/src/plugins/nsspellchecker/CMakeLists.txt
 @@ -7,6 +7,8 @@ add_library(sonnet_nsspellchecker MODULE ${sonnet_nsspellchecker_PART_SRCS})
- 
+
  target_link_libraries(sonnet_nsspellchecker PRIVATE KF5::SonnetCore "-framework Cocoa")
- 
+
 +target_compile_definitions(sonnet_nsspellchecker PRIVATE "QT_NO_EXCEPTIONS")
 +
- set_target_properties(sonnet_nsspellchecker PROPERTIES OUTPUT_NAME "nsspellchecker")
  install(TARGETS sonnet_nsspellchecker  DESTINATION ${KDE_INSTALL_PLUGINDIR}/kf5/sonnet/)
- 
